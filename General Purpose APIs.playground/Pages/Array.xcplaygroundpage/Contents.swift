fc90552e8e554e6985c4c6ca1eaef1f5899dc93b
Sean LH Lee <seanlh.lee@gmail.com>
2016-04-19 16:27:18 +0800

    minor change

--- General Purpose APIs.playground/Pages/Array.xcplaygroundpage/Contents.swift
+++ General Purpose APIs.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@ We can easily use a native map function to achieve.
 
 	let newArray = [1, 2, 3, 4].map{ $0 + 4 }		// [5, 6, 7, 8]
 
-But there is no easy way if the arry we want to handle is multi-dimational. For example, we have an 3x3 matrix form array **mat**. 
+If the arry we want to handle is multi-dimational, there are more steps we have to do. For example, we have a 3x3 matrix form array **mat**. 
 **mat** = [[1, 2, 3], [2, 4, 6], [5, 10, 15]]. 
 We want to get a new array in which each element array is divided by another array [1, 2, 3], i.e. for the first element [1, 2, 3] after operation the first sub-element is 1/1 and followed by 2/2, 3/3 . Finally, the new array is become [[1, 1, 1], [2, 2, 2], [5, 5, 5]]. We can easily find the operation is like `array.forEach{ $0 / [1, 2, 3] }`. We can write a closure to deal with two arrays to obtain a new array and become effective for each array.
 
